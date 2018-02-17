64d029212c8672636d3e3627583b7924daba8d52
hi-the-swan <jason.portenoy@gmail.com>
2018-02-17 10:14:38 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='jargon_distance',
+    version='0.1.0',
+    description='Calculate jargon distance metric between texts',
+    long_description=readme,
+    author='Jason Portenoy',
+    author_email='jason.portenoy@gmail.com',
+    url='https://github.com/h1-the-swan/jargon_distance',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=['six']
+)
+
